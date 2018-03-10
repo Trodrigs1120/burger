db1565099903227f342a91d8748093217cbf2457
trodrigs1120 <trodrigs@yahoo.com>
2018-03-10 10:50:07 -0500

    pre heroku commit

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ var connection = mysql.createConnection({
   user: "root",
 
   // Your password
-  password: "",
+  password: "44gxYhYftYebvd9fm",
   database: "burgers_db"
 });
 
--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,8 @@ CREATE database burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-      ID int NOT NULL AUTO_INCREMENT,
+      id int NOT NULL AUTO_INCREMENT,
       burger_name VARCHAR(100) NOT NULL,
       devoured boolean
+      PRIMARY KEY (id)
 )
